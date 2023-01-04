5cdcb6d2cac1e45d4bc5258e6c0738a52fb95617
mikedaltonmtl <michael.j.dalton@gmail.com>
2023-01-04 16:57:56 +0000

    add initial schema for database

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, phone) VALUES ('Alice', 5146729999);
+INSERT INTO users (name, phone) VALUES ('Kira', 6075552243);
