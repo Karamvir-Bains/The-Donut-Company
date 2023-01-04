61fd0486640ec9bbab42363191b1c7b1991511e1
mikedaltonmtl <michael.j.dalton@gmail.com>
2023-01-04 19:15:56 +0000

    add constants file for restaurant data

---
+++ public/constants.js
@@ -0,0 +1,14 @@
+const RESTAURANT_NAME = 'Donuts Inc.';
+const RESTAURANT_PHONE = 5556667890;
+const RESTAURANT_ADDRESS = {
+  street: '6300 Des Sources',
+  city: 'Montreal',
+  province: 'Quebec',
+  postcode: 'H9B 2N4'
+};
+
+module.exports = {
+  RESTAURANT_NAME,
+  RESTAURANT_PHONE,
+  RESTAURANT_ADDRESS
+};
