5c6dee98088fbdc05303bf90fce00d5f743fa7c3
mikedaltonmtl <michael.j.dalton@gmail.com>
2023-01-04 22:10:06 +0000

    add seed data and images

---
+++ db/seeds/04_orders_menu_items.sql
@@ -0,0 +1,12 @@
+-- Orders_menu_items table seeds
+
+-- (order_id 1 - 6)
+-- (item_id 1 - 12)
+
+INSERT INTO orders_menu_items (order_id, item_id, quantity) VALUES
+  (1, 2, 4), (1, 5, 1), (1, 12, 1),
+  (2, 1, 2), (2, 6, 2),
+  (3, 1, 10),
+  (4, 11, 2), (4, 3, 2), (4, 4, 1), (4, 7, 1),
+  (5, 8, 2), (5, 12, 1),
+  (6, 1, 6), (6, 12, 1);
