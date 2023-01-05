6c9583c667eeb8061a68e9cf2e3fef095e7548ac
Karamvir-Bains <kbains2000@gmail.com>
2023-01-05 10:57:43 -0700

    Integrate twilio api

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const twilioApiRoutes = require('./routes/twilio-api');
+const twilioRoutes = require('./routes/twilio');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -38,6 +40,8 @@ const usersRoutes = require('./routes/users');
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
+app.use('/api/twilio', twilioApiRoutes);
+app.use('/twilio', twilioRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
