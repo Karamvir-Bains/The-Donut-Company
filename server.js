ee624224f38e9df7f7ef8896dcc7f9bf867e8457
mikedaltonmtl <michael.j.dalton@gmail.com>
2023-01-06 15:41:03 +0000

    animate waiting spinner in status.ejs

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use('/users', usersRoutes);
 // Separate them into separate routes files (see above).
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('status');
 });
 
 app.listen(PORT, () => {
