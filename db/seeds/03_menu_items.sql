c6a22a18e9beb99045ca61d22dd8091cf2f03a8f
mikedaltonmtl <michael.j.dalton@gmail.com>
2023-01-10 21:25:46 +0000

    install cookie session

--- db/seeds/03_menu_items.sql
+++ db/seeds/03_menu_items.sql
@@ -1,23 +1,6 @@
 -- Menu-items table seeds
 
 INSERT INTO menu_items (name, img, price, description, preparation_time) VALUES
-<<<<<<< HEAD
-  ('Pink Sprinkle', 'pink_sprinkle.jpg', 499, 'Donut with pink frosting, topped with sprinkles.', 15),
-  ('Nutty Chocolate', 'choc_n_nut.jpg', 199, 'A donut frosted with chocolate and topped with nuts.', 15),
-  ('Traditional', 'traditional.jpg', 999, '4 assorted traditional donuts with various glazes.', 10),
-  ('Winter Warmth', 'winter_assortment.jpeg', 599, 'Assortment of 3 winteer-themed donuts.', 20),
-  ('St Valentine', 'valentine_assortment.jpeg', 699, 'Assortment of 4 heart-shaped donuts.', 30),
-  ('Birthday Assortment', 'assortment_12.jpg', 1199, 'Assortment of 12 donuts each uniquely frosted and topped.', 25),
-  ('Chocolate Top', 'choc_top.jpeg', 149, 'Vegan donut generously topped with chocolate.', 10),
-  ('Chocolate Vegan', 'choc_vegan.jpeg', 149, 'Chocolate pastry suitable for vegans.', 15),
-  ('Vegan Assortment', 'vegan_assortment.jpeg', 499, 'A three-pack of vegan donuts with various toppings.', 20),
-  ('Chocolate Sprinkle', 'choc_sprinkle.jpg', 449, 'Donut with chocolate frosting, topped with sprinkles.', 10),
-  ('Sugar Top', 'sugar_top.jpg', 299, '4 plain donuts each topped with sugar.', 10),
-  ('Sugar Glaze', 'sugar_glaze.jpg', 299, '4 plain donuts each glazed with sugar.', 10),
-  ('Double Choc', 'double_choc.jpg', 599, 'A three-pack of donuts frosted & topped with chocolate chunks.', 20),
-  ('4 Donut Assortment', 'assortment_4.jpg', 849, 'Assortment of 4 donuts each uniquely frosted and topped.', 20),
-  ('Multi-colour', 'multi_colour.jpg', 999, '12 frosted donuts with multi-coloured toppings.', 20);
-=======
   ('Pink Sprinkle', 'pink_sprinkle.jpg', 499, 'Iconic pink frosting, topped with sprinkles.', 15),
   ('Nutty Chocolate', 'choc_n_nut.jpg', 199, 'Chocolate glaze, topped with nuts.', 15),
   ('Sugar Top', 'sugar_top.jpg', 299, 'Topped with powdered sugar.', 10),
@@ -33,4 +16,3 @@ INSERT INTO menu_items (name, img, price, description, preparation_time) VALUES
   ('Assortment of 4', '4-pack.jpg', 449, 'Assortment of 4 donuts.', 10),
   ('Assortment of 6', '6-pack.jpg', 449, 'Assortment of 6 donuts.', 10),
   ('Assortment of 12', '12-pack.jpg', 449, 'Assortment of 12 donuts.', 10);
->>>>>>> network/performance
