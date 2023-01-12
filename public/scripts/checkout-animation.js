b4c8a0eab3c7961945bac1a4ccc6c97e3ccbf1aa
Karamvir-Bains <kbains2000@gmail.com>
2023-01-11 20:18:15 -0700

    Remove log from checkout-animation.js

--- public/scripts/checkout-animation.js
+++ public/scripts/checkout-animation.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $(window).scroll(function() {
     if ($(window).scrollTop() > 260) {
-      console.log($(window).scrollTop());
       $("#checkout").css({ top: '25em' });
     } else {
       $("#checkout").css({ top: '31em' });
