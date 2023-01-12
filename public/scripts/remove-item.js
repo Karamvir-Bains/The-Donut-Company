04c5a3728b990813ac1ee9a1882f568f2db1fcf8
Ehsan Elgendi <ehsan.elgendi@gmail.com>
2023-01-12 22:10:50 +0000

    update total order on btn with remove item

--- public/scripts/remove-item.js
+++ public/scripts/remove-item.js
@@ -3,6 +3,9 @@ $('#checkout_Items').on('click', '#remove_item', function(event) {
   event.preventDefault();
   console.log(this.parentElement);
   let itemName = $(this).parent().children('div.checkout-item-row').children('p#checkout_item_title').text();
+  let itemPrice = Number($(this).parent().children('div.checkout-item-row').children('p#checkout_item_price').text().slice(3));
+  let currentTotal = Number($(this).parent().parent().parent().parent().children('button#order-now').children('p#order-total').text().slice(3));
+  let newTotal = (currentTotal - itemPrice).toFixed(2);
   console.log(itemName);
 
   $.ajax({
@@ -12,6 +15,8 @@ $('#checkout_Items').on('click', '#remove_item', function(event) {
   })
   .done((response) => {
     console.log(response);
+    $('#order-total').text(`CA$${newTotal}`);
+
   });
 
 
