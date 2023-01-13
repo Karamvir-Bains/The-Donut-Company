f53af4da29a898df3fd0754e0ca13ca97088d082
Karamvir-Bains <kbains2000@gmail.com>
2023-01-13 11:29:40 -0700

    Add store-page image

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,23 +4,23 @@ $(document).ready(function() {
     method: 'GET',
     url: '/api/menu'
   })
-  .done((response) => {
-    const $menuItems = $('#menu-items');
-    // Variables to allow categories to be hard-coded
-    let count = 2;
-    const categories = ['Most Popular', 'Seasonal', 'Vegan', 'Gluten Free', 'Assortment'];
-    // iterate through the results of the menu items query then append the HTML
-    for (const i in response.items) {
+    .done((response) => {
+      const $menuItems = $('#menu-items');
+      // Variables to allow categories to be hard-coded
+      let count = 2;
+      const categories = ['Most Popular', 'Seasonal', 'Vegan', 'Gluten Free', 'Assortment'];
+      // iterate through the results of the menu items query then append the HTML
+      for (const i in response.items) {
       // Check for category
-      count++;
-      if (count % 3 === 0 && count < 18) {
-        const arrayIndex = (count / 3) - 1;
-        const category = $(`<li class="menu-category" id="${categories[arrayIndex]}">${categories[arrayIndex]}</li>`);
-        $menuItems.append(category);
-      }
-      // Add menu item
-      const item = response.items[i];
-      const menuItem = $(`
+        count++;
+        if (count % 3 === 0 && count < 18) {
+          const arrayIndex = (count / 3) - 1;
+          const category = $(`<li class="menu-category" id="${categories[arrayIndex]}">${categories[arrayIndex]}</li>`);
+          $menuItems.append(category);
+        }
+        // Add menu item
+        const item = response.items[i];
+        const menuItem = $(`
         <li id="itemId${item.id}" class="menu-item">
           <img class="donut-thumbnail" src="/images/${item.img}" alt="${item.name}">
           <p class="item-title">${item.name}</p>
@@ -31,15 +31,15 @@ $(document).ready(function() {
         $menuItems.append(menuItem);
       }
 
-    // Load pop-up when user clicks on menu item
-    $('.menu-item').on('click', function() {
-      const $itemPopup = $('#popup-item');
-      const itemSrc = $(this).children('img').attr('src');
-      const itemName = $(this).children('p.item-title').text();
-      const itemDesc = $(this).children('p.item-desc').text();
-      const itemPrice = Number($(this).children('p.item-price').text().slice(3));
-      let quantity = 1;
-      const popItem = $(`
+      // Load pop-up when user clicks on menu item
+      $('.menu-item').on('click', function() {
+        const $itemPopup = $('#popup-item');
+        const itemSrc = $(this).children('img').attr('src');
+        const itemName = $(this).children('p.item-title').text();
+        const itemDesc = $(this).children('p.item-desc').text();
+        const itemPrice = Number($(this).children('p.item-price').text().slice(3));
+        let quantity = 1;
+        const popItem = $(`
         <form class="popup" id="${this.id}" method="POST" action="/checkout">
           <p id="closePopup">X</p>
           <div class="popupContainer">
@@ -60,34 +60,34 @@ $(document).ready(function() {
       `); // popItem
         $itemPopup.append(popItem);
 
-      // Dim the background behing the popup using a full viewport div
-      const $popupBackground = $('#popup-background');
-      $popupBackground.css('display', 'block');
+        // Dim the background behing the popup using a full viewport div
+        const $popupBackground = $('#popup-background');
+        $popupBackground.css('display', 'block');
 
-      // Kill the popup if user clicks outside of it or on the close icon
-      const $closePopup = $('#closePopup');
-      $popupBackground.add($closePopup).on('click', function() {
-        $popupBackground.css('display', 'none');
-        $itemPopup.empty();
-      });
+        // Kill the popup if user clicks outside of it or on the close icon
+        const $closePopup = $('#closePopup');
+        $popupBackground.add($closePopup).on('click', function() {
+          $popupBackground.css('display', 'none');
+          $itemPopup.empty();
+        });
 
-      // Modify quantity and order amount when -/+ buttons are clicked
-      $('.quantityButton').on('click', function() {
-        const $button = $(this);
-        const oldValue = Number($('#donutQuantity').text());
-        let newValue = oldValue;
-        if ($button.text() == "+") {
-          newValue++;
-        } else {
-        // Don't allow decrementing below zero
-          if (oldValue > 0) {
-            newValue--;
+        // Modify quantity and order amount when -/+ buttons are clicked
+        $('.quantityButton').on('click', function() {
+          const $button = $(this);
+          const oldValue = Number($('#donutQuantity').text());
+          let newValue = oldValue;
+          if ($button.text() == "+") {
+            newValue++;
+          } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+              newValue--;
+            }
+            $('#donutQuantity').text(newValue);
+            // update the checkout button
+            $('#donutTotal').text(`CA$${(Math.round(itemPrice * newValue *100)/100).toFixed(2)}`);
           }
-          $('#donutQuantity').text(newValue);
-          // update the checkout button
-          $('#donutTotal').text(`CA$${(Math.round(itemPrice * newValue *100)/100).toFixed(2)}`);
         }); // .quantityButton item on click
-
       }); // .menu item on click
     }); // .done
 }); // document.ready
